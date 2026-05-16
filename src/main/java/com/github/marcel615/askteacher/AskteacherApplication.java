7e59afccc25c0c1edba5eedcceef7076511c8b20
marcel615 <alswo000615@gmail.com>
2026-05-16 12:07:44 +0900

    feat: AskTeacher 프로젝트 생성

---
+++ src/main/java/com/github/marcel615/askteacher/AskteacherApplication.java
@@ -0,0 +1,13 @@
+package com.github.marcel615.askteacher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AskteacherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AskteacherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/marcel615/askteacher/AskteacherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.marcel615.askteacher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AskteacherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
