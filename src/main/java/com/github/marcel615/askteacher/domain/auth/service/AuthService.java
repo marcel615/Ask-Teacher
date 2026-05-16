fa1e38ad4b348b7a84f16705662afdea3dde8354
marcel615 <alswo000615@gmail.com>
2026-05-16 15:46:33 +0900

    feat(user-signup): AuthService 클래스 구현
    - 인증 서비스 중 회원가입용 서비스 메서드 구현

---
+++ src/main/java/com/github/marcel615/askteacher/domain/auth/service/AuthService.java
@@ -0,0 +1,50 @@
+package com.github.marcel615.askteacher.domain.auth.service;
+
+import com.github.marcel615.askteacher.domain.auth.dto.SignupRequest;
+import com.github.marcel615.askteacher.domain.auth.dto.SignupResponse;
+import com.github.marcel615.askteacher.domain.user.entity.User;
+import com.github.marcel615.askteacher.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class AuthService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public SignupResponse signup(SignupRequest signupRequest) {
+        validateDuplicateEmail(signupRequest.getEmail());
+        validateDuplicateNickname(signupRequest.getNickname());
+
+        String encodedPassword = passwordEncoder.encode(signupRequest.getPassword());
+
+        User user = User.createUser(
+                signupRequest.getEmail(),
+                encodedPassword,
+                signupRequest.getNickname()
+        );
+
+        User savedUser = userRepository.save(user);
+
+        return SignupResponse.from(savedUser);
+    }
+
+
+    private void validateDuplicateEmail(String email) {
+        if(userRepository.existsByEmail(email)){
+            throw new IllegalArgumentException("이미 사용 중인 이메일입니다.");
+        }
+    }
+
+    private void validateDuplicateNickname(String nickname) {
+        if(userRepository.existsByNickname(nickname)){
+            throw new IllegalArgumentException("이미 사용 중인 닉네임입니다.");
+        }
+    }
+
+}
